eede3d619f66cbbca85244671f771642824674d7
Weverton <wevertonprocopio690@gmail.com>
2022-05-23 19:01:50 -0300

    feat: create user model

---
+++ backend/book-store.API/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace book_store.API.Models
+{
+    public class User
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string FullName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        public int Age { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+
+    }
+}
