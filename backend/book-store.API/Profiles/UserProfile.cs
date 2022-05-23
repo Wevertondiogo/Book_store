253d1b6e12d4912b7736225788e11464f1f06cbb
Weverton <wevertonprocopio690@gmail.com>
2022-05-23 20:32:15 -0300

    feat: implement dto

---
+++ backend/book-store.API/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using book_store.API.Data.Dtos.User;
+using book_store.API.Models;
+
+namespace book_store.API.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, ReadUserDTO>();
+        }
+    }
+}
